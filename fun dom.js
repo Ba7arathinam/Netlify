79a8a3b698df93ac93165885a67cae2b3ffc212b
BALARATHINAM T <109063403+Ba7arathinam@users.noreply.github.com>
2022-10-12 12:25:03 +0530

    Add files via upload

---
+++ fun dom.js
@@ -0,0 +1,8 @@
+function foo(){
+var fn=document.getElementById("fname").value;
+var mn=document.getElementById("mname").value;
+var ln=document.getElementById("lname").value;
+var em=document.getElementById("email").value;
+var ps=document.getElementById("pass").value;
+console.log(fn,mn,ln,em,ps);
+}
